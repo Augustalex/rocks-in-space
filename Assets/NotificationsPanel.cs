f2e0566c46bb54c40de23b17547845be71b86254
August Alexandersson <augustalexandersson@gmail.com>
2023-01-11 21:32:42 +0100

    ...

--- Assets/NotificationsPanel.cs
+++ Assets/NotificationsPanel.cs
@@ -1,15 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NotificationsPanel : MonoBehaviour
 {
     public GameObject notificationTemplate;
 
+    private readonly Queue<Notification> _notifications = new();
+
     void Start()
     {
         Notifications.Get().NotificationSent += OnNotificationsSent;
+
+        StartCoroutine(SlowUpdate());
+    }
+
+    private IEnumerator SlowUpdate()
+    {
+        while (gameObject != null)
+        {
+            if (_notifications.Count > 0)
+            {
+                ShowNotification(_notifications.Dequeue());
+            }
+
+            yield return new WaitForSeconds(1);
+        }
     }
 
     private void OnNotificationsSent(Notification notification)
+    {
+        _notifications.Enqueue(notification);
+    }
+
+    private void ShowNotification(Notification notification)
     {
         var notificationRoot = Instantiate(notificationTemplate, transform);
         var notificationPanel = notificationRoot.GetComponent<NotificationPanel>();
